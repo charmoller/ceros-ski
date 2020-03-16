5ebe71274119365a8a82aa31a413005165f2ca49
Charlotte Moller <cmoller@corridorinteractive.com>
2020-03-16 09:45:55 -0600

    Fix automated tests.

--- src/Test/Rhino.test.js
+++ src/Test/Rhino.test.js
@@ -2,8 +2,9 @@ import "babel-polyfill";
 import {Rhino} from "../Entities/Rhino";
 import * as Constants from "../Constants";
 import {SKIER_DIRECTIONS} from "../Constants";
+import {RHINO_RUN} from "../Constants";
 
 test('Create new rhino object', () => {
     const rhino = new Rhino(0, 0, SKIER_DIRECTIONS.DOWN);
-    expect(rhino).toEqual({assetName: Constants.RHINO_DEFAULT, direction: Constants.SKIER_DIRECTIONS.DOWN, speed: Constants.RHINO_STARTING_SPEED, x: 0, y: 0});
+    expect(rhino).toEqual({assetName: Constants.RHINO_RUN_ASSET[RHINO_RUN.RUN_1], direction: Constants.SKIER_DIRECTIONS.DOWN, eating: false, eatingAsset: Constants.RHINO_EAT.LIFT, eatingInterval: Constants.RHINO_EAT_TIME, previousDirection: Constants.SKIER_DIRECTIONS.DOWN, runningAsset: Constants.RHINO_RUN.RUN_1, runningInterval: Constants.RHINO_RUNNING_ANIMATE, turning: 0, speed: Constants.RHINO_STARTING_SPEED, x: 0, y: 0});
 });
--- src/Test/Skier.test.js
+++ src/Test/Skier.test.js
@@ -4,16 +4,16 @@ import * as Constants from "../Constants";
 
 test('Create new skier object', () => {
     const newSkier = new Skier(0, 0);
-    expect(newSkier).toEqual({assetName: Constants.SKIER_DOWN, direction: Constants.SKIER_DIRECTIONS.DOWN, jump: Constants.SKIER_JUMP.END, speed: Constants.SKIER_STARTING_SPEED, rhinoDelay: Constants.RHINO_DELAY, x: 0, y: 0});
+    expect(newSkier).toEqual({assetName: Constants.SKIER_DOWN, direction: Constants.SKIER_DIRECTIONS.DOWN, eaten: false, jump: Constants.SKIER_JUMP.END, speed: Constants.SKIER_STARTING_SPEED, rhinoDelay: Constants.RHINO_DELAY, x: 0, y: 0});
     expect(newSkier.isJumping()).toBe(false);
 });
 
 test('Crash skier then turn left', () => {
     const crashSkier = new Skier(0, 0);
     crashSkier.setDirection(Constants.SKIER_DIRECTIONS.CRASH);
-    expect(crashSkier).toEqual({assetName: Constants.SKIER_CRASH, direction: Constants.SKIER_DIRECTIONS.CRASH, jump: Constants.SKIER_JUMP.END, speed: Constants.SKIER_STARTING_SPEED, rhinoDelay: Constants.RHINO_DELAY, x: 0, y: 0});
+    expect(crashSkier).toEqual({assetName: Constants.SKIER_CRASH, direction: Constants.SKIER_DIRECTIONS.CRASH, eaten: false, jump: Constants.SKIER_JUMP.END, speed: Constants.SKIER_STARTING_SPEED, rhinoDelay: Constants.RHINO_DELAY, x: 0, y: 0});
     crashSkier.turnLeft();
-    expect(crashSkier).toEqual({assetName: Constants.SKIER_LEFT, direction: Constants.SKIER_DIRECTIONS.LEFT, jump: Constants.SKIER_JUMP.END, speed: Constants.SKIER_STARTING_SPEED, rhinoDelay: Constants.RHINO_DELAY, x: -Constants.SKIER_STARTING_SPEED, y: 0});
+    expect(crashSkier).toEqual({assetName: Constants.SKIER_LEFT, direction: Constants.SKIER_DIRECTIONS.LEFT, eaten: false, jump: Constants.SKIER_JUMP.END, speed: Constants.SKIER_STARTING_SPEED, rhinoDelay: Constants.RHINO_DELAY, x: -Constants.SKIER_STARTING_SPEED, y: 0});
 });
 
 test('Rhino is coming', () => {
